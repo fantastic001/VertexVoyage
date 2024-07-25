3ce7b8e45477346f7c10cfd6564f1c9b3c8b20ab
Stefan Nožinić <stefan@lugons.org>
2024-07-26 01:38:44 +0200

    Add optional parameter for specifying nodes in reconstruct function

--- vertex_voyage/reconstruction.py
+++ vertex_voyage/reconstruction.py
@@ -3,26 +3,32 @@ import networkx as nx
 import numpy as np 
 
 
-def reconstruct(k: int, embedding: list[np.array]) -> nx.Graph:
+def reconstruct(k: int, embedding: list[np.array], nodes = None) -> nx.Graph:
     """
     Reconstructs graph from embedding by taking k most closest nodes and creating links between them.
 
     Parameters:
     k (int): Number of closest nodes to connect.
     embedding (list[np.array]): List of embeddings of nodes.
+    nodes (list): List of nodes. If None, nodes are assumed to be [0, 1, ..., n].
 
     Returns:
     nx.Graph: Reconstructed undirected graph.
     """
     n = len(embedding)
     graph = nx.Graph()
-    for i in range(n):
-        graph.add_node(i)
+    if nodes is None:
+        for i in range(n):
+            graph.add_node(i)
+        nodes = list(range(n))
+    else:
+        for i, node in enumerate(nodes):
+            graph.add_node(node)
     distances = [] 
     for i in range(n):
             for j in range(i+1, n):
                 distances.append((np.linalg.norm(embedding[i] - embedding[j]), i, j))
     distances = sorted(distances, key=lambda x: x[0])
     for _, x, y in distances[:k]:
-        graph.add_edge(x, y)
+        graph.add_edge(nodes[x], nodes[y])
     return graph
