53fd80da23d4bca616152c6c4c5aa3e8c622feb1
Stefan Nožinić <stefan@lugons.org>
2024-07-30 18:23:53 +0200

    Add test for partitioning on sbm

--- vertex_voyage/test/test_partitioning.py
+++ vertex_voyage/test/test_partitioning.py
@@ -13,3 +13,14 @@ class TestPartitioning(unittest.TestCase):
         self.assertEqual(len(communities), partition_num)
         # approximately small differences between partitions )less than 10%_
         self.assertLessEqual(len(communities[0]), len(zachary.nodes) * 0.60)
+    
+    def test_partitioning_on_sbm(self):
+        n = 100
+        p = 0.1
+        q = 0.01
+        G = nx.planted_partition_graph(2, n, p, q, seed=42)
+        partition_num = 2
+        communities = partition_graph(G, partition_num)
+        self.assertEqual(len(communities), partition_num)
+        # approximately small differences between partitions )less than 10%_
+        self.assertLessEqual(len(communities[0]), len(G.nodes) * 0.60)
