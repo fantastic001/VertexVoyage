ed71f86bbd65ab581f5178cf07b7b91638ed84b4
Stefan Nožinić <stefan@lugons.org>
2024-08-09 03:42:18 +0200

    Add client module with methods for graph processing

---
+++ client/__main__.py
@@ -0,0 +1,5 @@
+
+from vertex_voyage.command_executor import * 
+from client.methods import Client
+
+command_executor_main(Client)
---
+++ client/methods.py
@@ -0,0 +1,28 @@
+
+from vertex_voyage.cluster import * 
+from vertex_voyage.command_executor import *
+
+class Client:
+    def create_graph(self, name: str, *, ip: str = "localhost"):
+        return do_rpc_client(ip, "create_graph", name=name)
+    def add_vertex(self, graph_name: str, vertex_name: str, *, ip: str = "localhost"):
+        return do_rpc_client(ip, "add_vertex", graph_name=graph_name, vertex_name=vertex_name)
+    def add_edge(self, graph_name: str, vertex1: str, vertex2: str, *, ip: str = "localhost"):
+        return do_rpc_client(ip, "add_edge", graph_name=graph_name, vertex1=vertex1, vertex2=vertex2)
+    def partition_graph(self, graph_name: str, num_nodes: str, *, ip: str="localhost"):
+        return do_rpc_client(ip, "partition_graph", graph_name=graph_name, num_nodes=num_nodes)
+    def get_node_num(self, graph_name: str, *, ip: str="localhost"):
+        return do_rpc_client(ip, "get_node_num", graph_name=graph_name)
+    def get_nodes(self, graph_name: str, *, ip: str="localhost"):
+        return do_rpc_client(ip, "get_nodes", graph_name=graph_name)
+    def get_partition(self, graph_name: str, partition_num: int, *, ip: str="localhost"):
+        return do_rpc_client(ip, "get_partition", graph_name=graph_name, partition_num=partition_num)
+    def process(self, graph_name: str, partition_num: int, *, ip: str="localhost"):
+        return do_rpc_client(ip, "process", graph_name=graph_name, partition_num=partition_num)
+    def get_graph(self, graph_name: str, *, ip: str = "localhost"):
+        return do_rpc_client(ip, "get_graph", graph_name=graph_name)
+    def get_leader(self, *, ip: str = "localhost"):
+        print("get_leader called")
+        return do_rpc_client(ip, "get_leader")
+        
+COMMAND_CLASSES = ["Client"]
