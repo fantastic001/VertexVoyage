27649571a03bcc71e1c162fd3f017842ef0c22c7
Stefan Nožinić <stefan@lugons.org>
2024-09-05 22:29:52 +0200

    Fix typo in analysis.sh script

--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -11,13 +11,13 @@ for PIPELINE in $PIPELINES; do
     for N_CLUSTERS in 2 3 4 5 6 7 8 9 10; do
         echo "Running pipeline $PIPELINE with $N_CLUSTERS clusters in $MODE mode"
         python -m client analyze_embeddings \
-            --single-node-results results/sequential/$PIPELINE/2_process.json \
-            --multi-node-results results/sequential/$PIPELINE/2_process.json \
+            --single-node-result results/sequential/$PIPELINE/2_process.json \
+            --multi-node-result results/sequential/$PIPELINE/2_process.json \
             --clusters $N_CLUSTERS \
             --output $RESULTS_FOLDER/$PIPELINE/$N_CLUSTERS/analysis_dim.csv
         python -m client analyze_embeddings \
-            --single-node-results results/sequential/$PIPELINE/3_process.json \
-            --multi-node-results results/sequential/$PIPELINE/3_process.json \
+            --single-node-result results/sequential/$PIPELINE/3_process.json \
+            --multi-node-result results/sequential/$PIPELINE/3_process.json \
             --clusters $N_CLUSTERS \
             --output $RESULTS_FOLDER/$PIPELINE/$N_CLUSTERS/analysis_n_walks.csv
     done
