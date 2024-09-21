c17ad37a1f4354bc8027269970b429df20b916ad
Stefan Nožinić <stefan@lugons.org>
2024-09-21 12:29:10 +0200

    Add new CSV files and generate script for presentation

---
+++ presentation/csv/generate.py
@@ -0,0 +1,20 @@
+
+import pandas as pd 
+import os 
+import sys 
+import matplotlib.pyplot as plt 
+
+path = sys.argv[1]
+
+images = ["4.3"] 
+
+for file in os.listdir(path):
+    if file.endswith('.csv'):
+        df = pd.read_csv(os.path.join(path, file))
+        basename = ".".join(file.split(".")[:-1])
+        if basename not in images:
+            print(df.to_latex(index=False, caption=basename, label=f"tab:{basename}"))
+        else:
+            df = df.set_index(df.columns[0])
+            df.plot.bar()
+            plt.savefig(os.path.join(path, f"{basename}.png"))
