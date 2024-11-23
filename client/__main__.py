9a2a9389ebdf345c1a043969495cdca58060e057
Stefan Nožinić <stefan@lugons.org>
2024-11-23 23:55:28 +0100

    Add main function to client and vertex_voyage modules

--- client/__main__.py
+++ client/__main__.py
@@ -1,5 +1,8 @@
 
 from vertex_voyage.command_executor import * 
 from client.methods import Client
+def main():
+    command_executor_main(Client)
 
-command_executor_main(Client)
+if __name__ == '__main__':
+    main()
--- vertex_voyage/__main__.py
+++ vertex_voyage/__main__.py
@@ -4,14 +4,11 @@ from vertex_voyage.cluster import register_node, get_binding_port
 from vertex_voyage.config import notify_plugins, get_config_int
 from vertex_voyage import ControlInterface
 import threading 
-notify_plugins("node_starting")
-register_node()
-notify_plugins("node_started")
-
-
-
-notify_plugins("register_commands", ControlInterface())
-
-
-
-command_executor_rpc(get_classes("vertex_voyage") + ControlInterface.additional_classes, get_binding_port())
+def main():
+    notify_plugins("node_starting")
+    register_node()
+    notify_plugins("node_started")
+    notify_plugins("register_commands", ControlInterface())
+    command_executor_rpc(get_classes("vertex_voyage") + ControlInterface.additional_classes, get_binding_port())
+if __name__ == '__main__':
+    main()
