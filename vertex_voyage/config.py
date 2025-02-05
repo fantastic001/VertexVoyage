ab7d6fbcbd80f84b40366bb9b493d0adefcf1cb8
Stefan Nožinić <stefan@lugons.org>
2025-02-05 19:42:38 +0100

    Refactor configuration and plugin loading

--- vertex_voyage/config.py
+++ vertex_voyage/config.py
@@ -1,25 +1,26 @@
 import os 
 import json
 import importlib 
+import inspect
 import sys 
 
-class VertexVoyageConfigurationError(Exception):
+class ConfigurationError(Exception):
     pass
 
-class VertexVoyageConflictError(Exception):
+class ConflictError(Exception):
     pass
 
-PROJECT_NAME = "vertex_voyage"
+# Use snake-case (.e.g. "my_project") for the project name
+PROJECT_NAME = "mila"
 PROJECT_VERSION = "0.1.0"
-PROJECT_ENVVAR_PREFIX = "VERTEX_VOYAGE"
+PROJECT_ENVVAR_PREFIX = PROJECT_NAME.upper()
+CONFIG_FILE_NAME = PROJECT_NAME + ".json"
 DEFAULT_PLUGINS = [
-    "vertex_voyage.plugins.greeting",
-    "vertex_voyage.plugins.config_commands",
-    "vertex_voyage.plugins.plugin_manager",
+    
 ]
 
 def get_config_location():
-    return os.environ.get(PROJECT_ENVVAR_PREFIX + "_CONFIG", os.path.join(os.path.expanduser("~"), ".vertex_voyage", "config.json"))
+    return os.environ.get(PROJECT_ENVVAR_PREFIX + "_CONFIG", os.path.join(os.path.expanduser("~"), ".config", CONFIG_FILE_NAME))
 
 def get_config(key: str, default, doc, expected_type=str):
     config = {} 
@@ -30,10 +31,10 @@ def get_config(key: str, default, doc, expected_type=str):
         if expected_type == list:
             return os.environ[envvar].split(" ")
         return expected_type(os.environ[envvar])
-    default_config_location = os.path.join(os.path.expanduser("~"), ".vertex_voyage", "config.json")
-    config_location = os.environ.get("VERTEX_VOYAGE_CONFIG", default_config_location)
+    default_config_location = os.path.join(os.path.expanduser("~"), ".config", CONFIG_FILE_NAME)
+    config_location = os.environ.get(PROJECT_ENVVAR_PREFIX +  "_CONFIG", default_config_location)
     if not os.path.exists(config_location) and config_location != default_config_location:
-        raise VertexVoyageConfigurationError(f"Configuration file {config_location} does not exist")
+        raise ConfigurationError(f"Configuration file {config_location} does not exist")
     if not os.path.exists(config_location):
         return expected_type(default)
     with open(config_location) as f:
@@ -50,7 +51,10 @@ def get_config_bool(key: str, default, doc) -> bool:
     return get_config(key, default, doc, expected_type=bool)
 
 def get_config_list(key: str, default, doc) -> list:
-    return get_config(key, default, doc, expected_type=list)
+    res =  get_config(key, default, doc, expected_type=list)
+    if res is None or res == "" or len(res) == 0 or res == [""]:
+        return default
+    return res
 
 def get_config_dict(key: str, default, doc) -> dict:
     return get_config(key, default, doc, expected_type=dict)
@@ -70,17 +74,48 @@ def set_config(key: str, value):
 
 def get_search_path():
     return get_config_list("plugin_search_path", [
-        "/usr/local/share/vertex_voyage/plugins",
-        "/usr/share/vertex_voyage/plugins",
-        os.path.join(os.path.expanduser("~"), ".vertex_voyage", "plugins")
+        "/usr/local/share/" + PROJECT_NAME + "/plugins",
+        "/usr/share/" + PROJECT_NAME + "/plugins",
+        os.path.join(os.path.expanduser("~"), "." + PROJECT_NAME, "plugins"),
+        os.path.join(os.path.expanduser("~"), ".local", "share", PROJECT_NAME, "plugins"),
+        os.getcwd()
     ], "List of plugin search paths")
 
 get_disabled_plugins = lambda: get_config_list("disabled_plugins", [], "List of plugins to disable")
+
+import importlib
+import pkgutil
+
+def _discover_submodules(package_name):
+    """
+    Given a string like 'my_package', discover and import all submodules of 'my_package'
+    even if they're not explicitly imported in my_package/__init__.py.
+    """
+    # First, import the base package
+    package = importlib.import_module(package_name)
+
+    # If the package has a __path__ attribute, we can walk its submodules
+    # (Packages almost always have __path__, unless it's a namespace pkg).
+    if not hasattr(package, '__path__'):
+        # It's not a package or does not have submodules in a normal sense
+        return []
+
+    submodules = []
+    for finder, modname, ispkg in pkgutil.walk_packages(package.__path__, package.__name__ + "."):
+        submodules.append(modname)
+
+    return submodules
+
+def _expand_plugin(plugin):
+    if plugin.endswith(".*"):
+        return [name for name in _discover_submodules(plugin[:-2])]
+    return [plugin]
 
 def list_plugins():
     plugins = DEFAULT_PLUGINS
     plugins += get_config_list("plugins", [], "List of plugins to load")
     disabled_plugins = get_disabled_plugins()
+    plugins = [plugin for p in plugins for plugin in _expand_plugin(p)]
     plugins = [plugin for plugin in plugins if plugin not in disabled_plugins]
     return list(set(plugins))
 
@@ -114,7 +149,25 @@ def get_plugin_result(method_name, *args, **kwargs):
         return None
     if len(found_results) == 1:
         return found_results[0][1]
-    raise VertexVoyageConflictError(f"Multiple plugins returned results for {method_name}: {found_results}")
+    raise ConflictError(f"Multiple plugins returned results for {method_name}: {found_results}")
+
+def get_symbols_satisfying(predicate):
+    plugins = load_plugins()
+    result = []
+    visited = set()
+    for name, plugin in plugins:
+        for symbol in dir(plugin):
+            if symbol in visited:
+                continue
+            visited.add(symbol)
+            if predicate(getattr(plugin, symbol)):
+                result.append(getattr(plugin, symbol))
+    return result
+
+def get_classes_inheriting(base_class):
+    return get_symbols_satisfying(lambda x: isinstance(x, type) and issubclass(x, base_class) and x != base_class)
+
+
 
 def pluggable(func):
     func_name = func.__name__
