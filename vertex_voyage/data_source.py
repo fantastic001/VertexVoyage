977b396ad4d62ced72eaa72143f0ff9b5e340ac1
Stefan Nožinić <stefan@lugons.org>
2025-02-06 02:11:09 +0100

    Add new data types to DataSource class

--- vertex_voyage/data_source.py
+++ vertex_voyage/data_source.py
@@ -1,8 +1,10 @@
 
 from vertex_voyage.config import get_classes_inheriting
 
+class DataType:
+    pass 
 class DataSource:
-    def get_data_type(self):
+    def get_data_type(self) -> DataType:
         return None
     
     def get_data(self):
@@ -14,6 +16,40 @@ class DataSource:
     def to_dict(self):
         return {}
     
+
+class TableData(DataType):
+    def __init__(self, columns) -> None:
+        super().__init__()
+        self.columns = columns
+
+class PanelData(DataType):
+    def __init__(self, columns, index) -> None:
+        super().__init__()
+        self.columns = columns
+        self.index = index
+
+class TimeSeriesData(DataType):
+    def __init__(self, index) -> None:
+        super().__init__()
+        self.index = index
+
+class GraphData(DataType):
+    def __init__(self, node_attrs, edge_attrs) -> None:
+        super().__init__()
+        self.node_attrs = node_attrs
+        self.edge_attrs = edge_attrs
+
+class ImageData(DataType):
+    def __init__(self, width, height) -> None:
+        super().__init__()
+        self.width = width
+        self.height = height
+
+class ArrayData(DataType):
+    def __init__(self, shape) -> None:
+        super().__init__()
+        self.shape = shape
+
 
 
 def load_data_source(class_name, params):
