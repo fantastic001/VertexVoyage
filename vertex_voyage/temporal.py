4131718217cdab693f5b56e9c16bfd7bbdae7cd7
Stefan Nožinić <stefan@lugons.org>
2025-03-19 18:44:08 +0100

    Implement temporal version of Barabasi-Albert model

--- vertex_voyage/temporal.py
+++ vertex_voyage/temporal.py
@@ -1,5 +1,6 @@
 
 from enum import Enum
+from random import random as rand
 
 class EventType(Enum):
     ADD = 1
@@ -22,6 +23,9 @@ class Event:
     
     def __str__(self):
         return f"Event({self.src}, {self.dest}, {self.timestamp}, {self.type}, {self.attrs})"
+
+    def __repr__(self):
+        return str(self)
 
 class EventSequence:
     def head(self) -> Event:
@@ -124,6 +128,82 @@ class FileEventSequence(EventSequence):
     
     def empty(self) -> bool:
         return self.is_empty
+
+class BarabasiAlbertEventSequence(EventSequence):
+    def __init__(self, nodes = None, degree = None, current = None, iter = 0):
+        if nodes:
+            self.nodes = nodes
+        else:
+            self.nodes = []
+        if degree:
+            self.degree = degree
+        else:
+            self.degree = {} 
+        if current:
+            self.current = current
+        else:
+            self.current = None  
+        self.iter = iter
+    
+    def head(self) -> Event:
+        if self.current is None:
+            if len(self.nodes) == 0:
+                mynode = 0
+            else:
+                mynode = max(self.nodes) + 1
+            self.nodes.append(mynode)
+            self.degree[mynode] = 0
+            self.current = self.nodes[0]
+            if sum(self.degree.values()) == 0:
+                p = 0.5
+            else:
+                p = self.degree[self.current] / sum(self.degree.values())
+            if rand() < p:
+                return Event(
+                    self.current, 
+                    mynode, 
+                    self.iter,
+                    EventType.ADD,
+                    {}
+                )
+            else:
+               self.current = self.nodes[self.nodes.index(self.current) + 1]
+               self.iter += 1
+               return self.head()
+        else:
+            if self.current == self.nodes[-1]:
+                self.current = None 
+                self.iter += 1
+                return self.head()
+            else:
+                if sum(self.degree.values()) == 0:
+                    p = .5
+                else:
+                    p = self.degree[self.current] / sum(self.degree.values())
+                if rand() < p:
+                    return Event(
+                        self.current,
+                        self.nodes[-1],
+                        self.iter,
+                        EventType.ADD,
+                        {}
+                    )
+                else:
+                    self.current = self.nodes[self.nodes.index(self.current) + 1]
+                    return self.head()
+
+
+    def tail(self) -> "BarabasiAlbertEventSequence":
+        if self.current == self.nodes[-1]:
+            return BarabasiAlbertEventSequence(self.nodes, self.degree, None, self.iter + 1)
+        else:
+            return BarabasiAlbertEventSequence(self.nodes, self.degree, self.nodes[self.nodes.index(self.current) + 1], self.iter + 1)
+
+    def append(self, event: Event) -> "BarabasiAlbertEventSequence":
+        raise NotImplementedError("Cannot append to BarabasiAlbertEventSequence")
+
+    def empty(self) -> bool:
+        return False
 
 class EventStream:
     def __init__(self, sequence: EventSequence):
