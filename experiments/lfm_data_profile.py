844c082cef40ca5a649858381048e05986fbb9b8
Stefan Nožinić <stefan@lugons.org>
2025-05-02 18:32:13 +0200

    Fix data profile benchmark for LFM

--- experiments/lfm_data_profile.py
+++ experiments/lfm_data_profile.py
@@ -1,10 +1,11 @@
 
 from vertex_voyage.benchmark_base import Benchmark
-from vertex_voyage.benchmark import data_profile,  display_data_profile
-from vertex_voyage.partitioning import modified__lfm
+from vertex_voyage.benchmark import data_profile,  display_data_profile, CallCountingMock
+from vertex_voyage.partitioning import modified__lfm, Community
 import networkx as nx 
 import pandas as pd 
 import numpy as np
+import vertex_voyage.partitioning
 class LFMDataProfile(Benchmark):
     """
     This class performs a data profile on the LFM problem.
@@ -15,16 +16,17 @@ class LFMDataProfile(Benchmark):
 
     def run(self,results_path):
         def lfm_partitioner(graph):
-            return modified__lfm(graph, 5, pm_k=1000)
+            result =  modified__lfm(graph, 5, pm_k=1000)
+            return result
         problems = []
-        for l in range(10, 100, 20):
+        for l in range(10, 60, 20):
             for k in range(2, 10, 2):
-                for p in np.arange(0.1, 1.1, 0.2):
+                for p in np.arange(0.1, 1.1, 0.1):
                     for q in np.arange(.1, .5, 0.1):
                         problems.append(lambda: nx.planted_partition_graph(l, k, p, p*q))
         df = data_profile({
             "LFM": lfm_partitioner
-        }, "vertex_voyage.partitioning.remove_vertex_from_community", problems, self)
+        }, (vertex_voyage.partitioning, "remove_vertex_from_community"), problems, self)
         df.to_csv(f"{results_path}/results.csv", index=False)
 
     def display(self, results_path):
