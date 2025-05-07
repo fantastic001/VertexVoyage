221b186f5c798f423f059b4089f8f03a19c17e1c
Stefan Nožinić <stefan@lugons.org>
2025-05-07 14:39:16 +0200

    Refactor benchmark functions and enhance WindowedLPSBM class for improved edge cut and balance calculations

--- experiments/windowed_label_propagation.py
+++ experiments/windowed_label_propagation.py
@@ -12,50 +12,88 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 import os 
 
+def benchmark_partitioner(benchmark, part_num, partitioner_class,  *args, graph_generator = None):
+    data = [] 
+    sbm_matrix = [] 
+    p = 0.5
+    community_count = 5 
+    iterations = 50
+    for i in range(community_count):
+        sbm_matrix.append([p if i == j else (1 - p) / (community_count - 1) for j in range(community_count)])
+    for k in range(iterations):
+        if graph_generator:
+            g = list(graph_generator())
+        else:
+            g = list(FirstN(ShuffledSequence(SBMSequence([1 / community_count] * community_count, sbm_matrix), 1000), 10000))
+        partitioner = partitioner_class(part_num, *args)
+        for i, matrix in enumerate(edge_cut_matrix(g, partitioner)):
+            edge_cut = sum(matrix[a,b] for a in range(part_num) for b in range(part_num) if a > b)
+            edge_cut = edge_cut / sum(matrix[a,b] for a in range(part_num) for b in range(part_num) if a >= b)
+            data.append({
+                "time": i,
+                "edge_cut": edge_cut,
+            })
+        partitioner = partitioner_class(part_num, *args)
+        for i, partition_sizes_ in enumerate(partition_sizes(g, partitioner)):
+            data[i]["balance"] = get_partition_average_balance(partition_sizes_, part_num)
+        if benchmark:
+            benchmark.report_progress(k+1, iterations)
+    return pd.DataFrame(data)
+
+def display_benchmark_results(df, x_label, y_labels):
+    plt.figure(figsize=(10, 6))
+    x = df[x_label].unique()
+    ys = [df.groupby(x_label)[y].mean() for y in y_labels]
+    for y, label in zip(ys, y_labels):
+        plt.plot(x, y, label=label)
+    plt.xlabel(x_label)
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
 class WindowedLPSBM(Benchmark):
     
     NAME = "Windowed Label Propagation with SBM over time"
 
     def run(self, results_folder):
-        data = [] 
-        sbm_matrix = [] 
-        part_num = 16
-        p = 0.5
-        community_count = 5 
-        window_size = 5000
-        iterations = 30
-        for i in range(community_count):
-            sbm_matrix.append([p if i == j else (1 - p) / (community_count - 1) for j in range(community_count)])
-        for k in range(iterations):
-            g = list(FirstN(ShuffledSequence(SBMSequence([1 / community_count] * community_count, sbm_matrix), 1000), 10000))
-            partitioner = WindowedLabelPropagationTemporalGraphPartitioner(part_num, window_size)
-            total_edges = len(g)
-            for i, matrix in enumerate(edge_cut_matrix(g, partitioner)):
-                edge_cut = sum(matrix[a,b] for a in range(part_num) for b in range(part_num) if a > b)
-                edge_cut = edge_cut / sum(matrix[a,b] for a in range(part_num) for b in range(part_num) if a >= b)
-                data.append({
-                    "time": i,
-                    "edge_cut": edge_cut,
-                    "edge_cut_ratio": edge_cut,
-                })
-            partitioner = WindowedLabelPropagationTemporalGraphPartitioner(part_num, window_size)
-            for i, partition_sizes_ in enumerate(partition_sizes(g, partitioner)):
-                data[i]["balance"] = get_partition_average_balance(partition_sizes_, part_num)
-            self.report_progress(k+1, iterations)
+        window_size = 1000
+        df = benchmark_partitioner(
+            self, 
+            16, 
+            WindowedLabelPropagationTemporalGraphPartitioner,
+            window_size
+        )
+        df.to_csv(os.path.join(results_folder, "windowed_label_propagation.csv"), index=False)
+    
+    def display(self, results_folder):
+        df = pd.read_csv(os.path.join(results_folder, "windowed_label_propagation.csv"))
+        display_benchmark_results(df, "time", ["edge_cut", "balance"])
+
+
+class WindowedLPSBM(Benchmark):
+    
+    NAME = "Windowed Label Propagation with SBM - window size"
+
+    def run(self, results_folder):
+        samples = range(100, 10000, 100)
+        data = []
+        for i, window_size in enumerate(samples):
+            df_part = benchmark_partitioner(
+                None, 
+                16, 
+                WindowedLabelPropagationTemporalGraphPartitioner,
+                window_size,
+                graph_generator = lambda: FirstN(ShuffledSequence(SBMSequence([1 / 5] * 5, [[0.5 if i == j else 0.1 for j in range(5)] for i in range(5)]), 100), 1000)
+            )
+            data.append({
+                "window_size": window_size,
+                "edge_cut": df_part[df_part.time == 900]["edge_cut"].mean(),
+                "balance": df_part[df_part.time == 900]["balance"].mean()
+            })
+            self.report_progress(i+1, len(samples))
         df = pd.DataFrame(data)
         df.to_csv(os.path.join(results_folder, "windowed_label_propagation.csv"), index=False)
     
     def display(self, results_folder):
         df = pd.read_csv(os.path.join(results_folder, "windowed_label_propagation.csv"))
-        plt.figure(figsize=(10, 6))
-        x = df["time"].unique()
-        y = df.groupby("time")["edge_cut"].mean()
-        z = df.groupby("time")["balance"].mean()
-        plt.plot(x, y, label="Edge Cut", color='blue')
-        plt.plot(x, z, label="Balance", color='orange')
-        plt.xlabel("Time")
-        plt.ylabel("Edge Cut")
-        plt.title("Label Propagation Edge Cut Over Time")
-        plt.grid(True)
-        plt.legend()
-        plt.show()
+        display_benchmark_results(df, "window_size", ["edge_cut", "balance"])
