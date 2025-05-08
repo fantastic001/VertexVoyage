ab6679d0b477c941fdcacfa7994e3503c0ee8b7d
Stefan Nožinić <stefan@lugons.org>
2025-05-08 16:11:17 +0200

    Implement failfast option in unittest runner and enhance DebugTestResult class in order to catch errors in debug mode

--- vertex_voyage/test/__main__.py
+++ vertex_voyage/test/__main__.py
@@ -1,9 +1,28 @@
 import unittest
+import os 
+
+class DebugTestResult(unittest.TextTestResult):
+    def __init__(self, *args, failfast=False, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.failfast = failfast
+    def addFailure(self, test, err):
+        super().addFailure(test, err)
+        if self.failfast:
+            # Raise the exception to trigger debugger
+            raise err[1].with_traceback(err[2])
+
+    def addError(self, test, err):
+        super().addError(test, err)
+        if self.failfast:
+            # Raise the exception to trigger debugger
+            raise err[1].with_traceback(err[2])
+
 
 if __name__ == '__main__':
+    failfast = os.getenv('FAILFAST', '0') == '1'
     loader = unittest.TestLoader()
     start_dir = 'vertex_voyage/test'
     suite = loader.discover(start_dir)
 
-    runner = unittest.TextTestRunner()
+    runner = unittest.TextTestRunner(resultclass=DebugTestResult, failfast=failfast)
     runner.run(suite)
