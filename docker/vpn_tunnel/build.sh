3919db2aa1f0ac744f2108e40172409886f18c0a
Stefan Nožinić <stefan@lugons.org>
2025-05-14 11:22:36 +0200

    Add ARM architecture detection and set Docker platform accordingly

--- docker/vpn_tunnel/build.sh
+++ docker/vpn_tunnel/build.sh
@@ -2,6 +2,19 @@
 
 THIS_DIR=$(readlink -f $(dirname $0))
 
+is_arm() {
+    if [ "$(uname -m)" == "arm64" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 INSTALL_DIR=$THIS_DIR/install
+
+if is_arm; then 
+    export DOCKER_DEFAULT_PLATFORM=linux/amd64
+fi 
+
 
 docker build -t anyconnect -f $THIS_DIR/Dockerfile $THIS_DIR
