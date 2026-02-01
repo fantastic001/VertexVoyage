74ab7f4df77004bbb4de7075ec85abe925adc1d7
Stefan Nožinić <stefan@lugons.org>
2026-02-01 18:04:37 +0100

    Refactor run_experiments.sh to include dataset iteration and execution of experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,4 +2,28 @@
 
 export VERTEX_VOYAGE_PLUGINS="experiments.*"
 
-python -m vertex_voyage.benchmark 
+# python -m vertex_voyage.benchmark 
+
+
+if [ "$DEBUG" = "1" ]; then
+    echo "Running in DEBUG mode"
+    set -x
+fi
+
+for dataset in zachary AstroPh CITESEER Cit-HepPh Cit-HepTh; do 
+    for parts in 1 2 4 8 16; do 
+        echo "----------------------------------------"
+        echo "Running experiment on dataset $dataset with $parts partitions"
+        python -m vertex_voyage  test \
+            --name $dataset \
+            --partitions $parts \
+            --break-early \
+            --use-dataset-params \
+            --long-run \
+            --use-lpa \
+            --epochs 10 
+        echo "----------------------------------------"
+    done
+done
+
+echo "All experiments completed."
